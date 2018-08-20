4e3909149ae3aaf0ad863963e2b272566e5c9d10
HaiNguyenPV12 <39506270+HaiNguyenPV12@users.noreply.github.com>
2018-08-20 20:52:53 +0700

    scripts for contact html

---
+++ Scripts/contact.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('contact', []);
+app.controller('contactcontroller', function ($scope) {
+    //document.title = 'Contact Us';
+    $('form').on('submit', function () {
+        $('#ctModal').modal('show');
+        return false;
+    });
+    $scope.emFunc = function () {
+        $scope.contactname = $scope.ctName;
+        $scope.contactemail = $scope.ctEmail;
+    };
+});
