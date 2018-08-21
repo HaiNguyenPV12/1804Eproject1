b5f5b1aad03972f0e32c9fbeb405f06fded860ed
HaiNguyenPV12 <39506270+HaiNguyenPV12@users.noreply.github.com>
2018-08-21 21:57:40 +0700

    TOC scripts for course pages

---
+++ Scripts/courses.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    if ($('#toccontents').is(":visible")) {
+        $('#toctoggle').html('[hide]');
+    }
+    $('#toctoggle').click(function () {
+        $('#toccontents').toggle(75, function () {
+            var isHidden = $('#toccontents').is(":hidden");
+            var isVisible = $('#toccontents').is(":visible");
+            if (isHidden) {
+                $('#toctoggle').html('[show]');
+            }
+            if (isVisible) {
+                $('#toctoggle').html('[hide]');
+            }
+        });
+    });
+});
+
